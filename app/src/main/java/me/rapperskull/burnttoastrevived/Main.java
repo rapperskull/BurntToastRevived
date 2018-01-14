147c41d29d1bf5f0e9c989a5920ea3ada13e296a
Rapper_skull <rapperskull.mail@gmail.com>
2018-01-14 14:23:29 +0100

    Initial release

---
+++ app/src/main/java/me/rapperskull/burnttoastrevived/Main.java
@@ -0,0 +1,66 @@
+package me.rapperskull.burnttoastrevived;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.content.res.XResources;
+import android.view.Gravity;
+import android.widget.GridLayout.LayoutParams;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.callbacks.XC_LayoutInflated;
+
+public class Main implements IXposedHookZygoteInit {
+
+    @Override
+    public void initZygote(StartupParam startupParam) throws Throwable {
+        XC_LayoutInflated hook = new XC_LayoutInflated() {
+            @Override
+            public void handleLayoutInflated(LayoutInflatedParam liparam) throws Throwable {
+                LinearLayout layout = (LinearLayout) liparam.view;
+                Context context = layout.getContext();
+                TextView view = (TextView) liparam.view.findViewById(android.R.id.message);
+
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                        LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+                params.gravity = Gravity.CENTER;
+                params.rightMargin = 20;
+
+                view.setLayoutParams(params);
+
+                PackageManager pm = context.getPackageManager();
+
+                ImageView imageView = new ImageView(context);
+                imageView.setMaxHeight(view.getHeight() + 128);
+                imageView.setMaxWidth(view.getHeight() + 128);
+                imageView.setAdjustViewBounds(true);
+                imageView.setImageDrawable(context.getApplicationInfo().loadIcon(pm));
+                LinearLayout.LayoutParams params1 = new LinearLayout.LayoutParams(
+                        LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+                params1.gravity = Gravity.CENTER;
+                params1.topMargin = 15;
+                params1.bottomMargin = 15;
+                params1.rightMargin = 10;
+                params1.leftMargin = 20;
+                imageView.setLayoutParams(params1);
+
+                layout.setOrientation(LinearLayout.HORIZONTAL);
+                layout.addView(imageView, 0);
+            }
+        };
+
+        XResources.hookSystemWideLayout("android", "layout", "transient_notification", hook);
+        try {
+            XResources.hookSystemWideLayout("android", "layout", "tw_transient_notification", hook);
+        } catch (Resources.NotFoundException e) {
+
+
+        } catch (Throwable t) {
+
+        }
+    }
+
+}
