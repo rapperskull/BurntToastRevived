b5f7d1767d3f034354b0e5c1d36c197552f6cc86
Rapper_skull <rapperskull.mail@gmail.com>
2018-01-21 16:04:41 +0100

    Added option to change icon size Added support for some ROMs with different layout for toast messages

--- app/src/main/java/me/rapperskull/burnttoastrevived/Main.java
+++ app/src/main/java/me/rapperskull/burnttoastrevived/Main.java
@@ -1,5 +1,6 @@
 package me.rapperskull.burnttoastrevived;
 
+import android.os.Build;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
@@ -8,47 +9,127 @@ import android.view.Gravity;
 import android.widget.GridLayout.LayoutParams;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import java.io.File;
 
 import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.callbacks.XC_LayoutInflated;
+import de.robv.android.xposed.XSharedPreferences;
+import de.robv.android.xposed.XposedBridge;
 
 public class Main implements IXposedHookZygoteInit {
+    private XSharedPreferences pref;
+    private int size;
+    private Context context;
+    private int LateralMargin=0;
+    private int VerticalMargin=0;
+    private int CentralMargin=0;
+    final boolean isN=(android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.N);
+    private static final File prefsFile = new File("/data/user_de/0/me.rapperskull.burnttoastrevived/shared_prefs/settings.xml");
 
     @Override
     public void initZygote(StartupParam startupParam) throws Throwable {
         XC_LayoutInflated hook = new XC_LayoutInflated() {
             @Override
             public void handleLayoutInflated(LayoutInflatedParam liparam) throws Throwable {
-                LinearLayout layout = (LinearLayout) liparam.view;
-                Context context = layout.getContext();
-                TextView view = (TextView) liparam.view.findViewById(android.R.id.message);
 
-                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                        LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-                params.gravity = Gravity.CENTER;
-                params.rightMargin = 20;
+                commonScripts();
 
-                view.setLayoutParams(params);
+                if(liparam.view instanceof LinearLayout){
+                    XposedBridge.log("Burnt Toast Revived: Found LinearLayout");
 
-                PackageManager pm = context.getPackageManager();
+                    if (isN){
+                        LateralMargin = 20;
+                        VerticalMargin = 15;
+                        CentralMargin = 10;
+                    } else {
+                        LateralMargin = 0;
+                        VerticalMargin = 0;
+                        CentralMargin = 10;
+                    }
 
-                ImageView imageView = new ImageView(context);
-                imageView.setMaxHeight(view.getHeight() + 128);
-                imageView.setMaxWidth(view.getHeight() + 128);
-                imageView.setAdjustViewBounds(true);
-                imageView.setImageDrawable(context.getApplicationInfo().loadIcon(pm));
-                LinearLayout.LayoutParams params1 = new LinearLayout.LayoutParams(
-                        LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-                params1.gravity = Gravity.CENTER;
-                params1.topMargin = 15;
-                params1.bottomMargin = 15;
-                params1.rightMargin = 10;
-                params1.leftMargin = 20;
-                imageView.setLayoutParams(params1);
+                    LinearLayout layout = (LinearLayout) liparam.view;
+                    context = layout.getContext();
+                    TextView view = (TextView) liparam.view.findViewById(android.R.id.message);
 
-                layout.setOrientation(LinearLayout.HORIZONTAL);
-                layout.addView(imageView, 0);
+                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                            LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+                    params.gravity = Gravity.CENTER;
+                    params.rightMargin = LateralMargin;
+
+                    view.setLayoutParams(params);
+
+                    PackageManager pm = context.getPackageManager();
+
+                    ImageView imageView = new ImageView(context);
+                    imageView.setMaxHeight(view.getHeight() + size);
+                    imageView.setMaxWidth(view.getHeight() + size);
+                    imageView.setAdjustViewBounds(true);
+                    imageView.setImageDrawable(context.getApplicationInfo().loadIcon(pm));
+
+                    LinearLayout.LayoutParams params1 = new LinearLayout.LayoutParams(
+                            LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+                    params1.gravity = Gravity.CENTER;
+                    params1.setMargins(LateralMargin,VerticalMargin,CentralMargin,VerticalMargin);
+                    imageView.setLayoutParams(params1);
+
+                    layout.setOrientation(LinearLayout.HORIZONTAL);
+                    layout.addView(imageView, 0);
+
+                } else if(liparam.view instanceof RelativeLayout){
+                    XposedBridge.log("Burnt Toast Revived: Found RelativeLayout");
+
+                    LateralMargin = 0;
+                    VerticalMargin = 0;
+                    CentralMargin = 10;
+
+                    RelativeLayout layout = (RelativeLayout) liparam.view;
+                    context = layout.getContext();
+                    TextView view = (TextView) liparam.view.findViewById(android.R.id.message);
+
+                    layout.setBackground(view.getBackground());
+
+                    LinearLayout inner = new LinearLayout(context);
+                    RelativeLayout.LayoutParams innerParams = new RelativeLayout.LayoutParams(
+                            LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+                    innerParams.addRule(RelativeLayout.CENTER_IN_PARENT , RelativeLayout.TRUE);
+                    innerParams.setMargins(0,0,0,0);
+                    inner.setLayoutParams(innerParams);
+
+                    PackageManager pm = context.getPackageManager();
+
+                    ImageView imageView = new ImageView(context);
+                    imageView.setMaxHeight(view.getHeight() + size);
+                    imageView.setMaxWidth(view.getHeight() + size);
+                    imageView.setAdjustViewBounds(true);
+                    imageView.setImageDrawable(context.getApplicationInfo().loadIcon(pm));
+
+                    LinearLayout.LayoutParams params1 = new LinearLayout.LayoutParams(
+                            LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+                    params1.gravity = Gravity.CENTER;
+                    params1.setMargins(LateralMargin,VerticalMargin,CentralMargin,VerticalMargin);
+                    imageView.setLayoutParams(params1);
+
+                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                            LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+                    params.gravity = Gravity.CENTER;
+                    params.rightMargin = LateralMargin;
+                    params.leftMargin = 0;
+
+                    view.setLayoutParams(params);
+                    view.setBackground(null);
+
+                    layout.removeAllViews();
+                    inner.setOrientation(LinearLayout.HORIZONTAL);
+                    inner.addView(imageView, 0);
+                    inner.addView(view, 1);
+                    layout.addView(inner, 0);
+
+                } else {
+                    XposedBridge.log("Burnt Toast Revived: Unknown Layout");
+                }
+
             }
         };
 
@@ -61,6 +142,18 @@ public class Main implements IXposedHookZygoteInit {
         } catch (Throwable t) {
 
         }
+    }
+
+    private void commonScripts(){
+        if (isN) {
+            pref = new XSharedPreferences(prefsFile);
+        } else {
+            pref = new XSharedPreferences(Main.class.getPackage().getName(), "settings");
+        }
+        pref.getFile().setReadable(true, false);
+        pref.reload();
+        size = pref.getInt("icon_size", 128);
+        XposedBridge.log("Burnt Toast Revived: Got size of " + Integer.toString(size));
     }
 
 }
---
+++ app/src/main/java/me/rapperskull/burnttoastrevived/MainActivity.java
@@ -0,0 +1,150 @@
+package me.rapperskull.burnttoastrevived;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.Gravity;
+import android.widget.SeekBar;
+import android.widget.Toast;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+
+import java.io.File;
+
+import de.robv.android.xposed.XposedBridge;
+
+public class MainActivity extends AppCompatActivity {
+    final boolean isN=(android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.N);
+    private Context context;
+    static String mPrefsName="settings";
+    final int baseSize=32;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        context = this.getBaseContext();
+        if(isN){
+            context = !context.isDeviceProtectedStorage() ? context.createDeviceProtectedStorageContext() : context;
+        }
+
+        final SharedPreferences sharedPref = getPreferencesAndKeepItReadable(context, mPrefsName);
+        int storedValue = sharedPref.getInt("icon_size",128);
+
+        SeekBar mySeekBar = (SeekBar) findViewById(R.id.seekBar);
+        if(storedValue>0){
+            mySeekBar.setProgress(sizeToSlide(storedValue));
+        }
+
+        mySeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) {
+                savePreferences(slideToSize(progress),sharedPref);
+            }
+        });
+
+    }
+
+    private void savePreferences(int newValue, SharedPreferences sharedPref){
+        if(newValue>0){
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putInt("icon_size", newValue);
+            editor.commit();
+            generateToast("Set size to " + Integer.toString(newValue),true);
+        } else {
+            XposedBridge.log("Burnt Toast Revived: Tried to set a value of " + Integer.toString(newValue));
+        }
+    }
+
+    private void generateToast(String message, boolean center){
+        Toast toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+        if(center) {
+            toast.setGravity(Gravity.CENTER, 0, 0);
+        }
+        toast.show();
+    }
+
+    private void generateToast(String message){
+        generateToast(message,false);
+    }
+
+    public SharedPreferences getPreferencesAndKeepItReadable(Context ctx, String prefName) {
+        SharedPreferences prefs;
+        if(isN){
+            prefs = ctx.getSharedPreferences(prefName, 0);
+        } else {
+            prefs = ctx.getSharedPreferences(prefName, MODE_WORLD_READABLE);
+        }
+        createFile(ctx, prefName);
+        fixPermissions(ctx, prefName);
+        return prefs;
+    }
+
+    private void createFile(Context ctx, String prefName) {
+        try {
+            File appBaseDir;
+            if(isN){
+                appBaseDir = new File(ctx.getDataDir().getAbsolutePath());
+            } else {
+                appBaseDir = new File(ctx.getFilesDir() + "/..");
+            }
+            appBaseDir.setExecutable(true, false);
+            appBaseDir.setReadable(true, false);
+
+            File sharedPrefsFolder = new File(appBaseDir.getAbsolutePath() + "/shared_prefs");
+            if (!sharedPrefsFolder.exists()) {
+                sharedPrefsFolder.mkdir();
+                sharedPrefsFolder.setExecutable(true, false);
+                sharedPrefsFolder.setReadable(true, false);
+            }
+
+            File f = new File(sharedPrefsFolder.getAbsolutePath() + "/" + prefName + ".xml");
+            if (!f.exists()) {
+                f.createNewFile();
+                f.setReadable(true, false);
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+    private void fixPermissions(Context ctx, String prefName) {
+        File appBaseDir;
+        if (isN) {
+            appBaseDir = new File(ctx.getDataDir().getAbsolutePath());
+        } else {
+            appBaseDir = new File(ctx.getFilesDir() + "/..");
+        }
+        appBaseDir.setExecutable(true, false);
+        appBaseDir.setReadable(true, false);
+
+        File sharedPrefsFolder = new File(appBaseDir.getAbsolutePath() + "/shared_prefs");
+        if (sharedPrefsFolder.exists()) {
+            sharedPrefsFolder.setExecutable(true, false);
+            sharedPrefsFolder.setReadable(true, false);
+
+            File f = new File(sharedPrefsFolder.getAbsolutePath() + "/" + prefName + ".xml");
+            if (f.exists()) {
+                f.setExecutable(true, false);
+                f.setReadable(true, false);
+            }
+        }
+    }
+
+    private int slideToSize(int slide){
+        return baseSize*(slide+1);
+    }
+    private int sizeToSlide(int size){
+        return Math.round((size/baseSize)-1);
+    }
+
+}
